5e042a265d8b42505efb3406c0f1e970ec583d0b
Levkov Gleb <Glebchanskiy@yandex.by>
2023-04-18 08:46:00 +0300

    update: added problem solver support

--- pancake.sh
+++ pancake.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+WORKDIR=$(PWD)
+
 function usage() {
     cat <<USAGE
 
@@ -15,6 +17,51 @@ USAGE
     exit 1
 }
 
+function prepare_sc_machine() {
+    echo -e "\033[1mSC-MACHINE\033[0m":
+    if [ -e "sc-machine" ]; then
+        cd sc-machine
+        git pull
+    else
+         git clone https://github.com/ostis-ai/sc-machine
+    fi
+    cd $WORKDIR
+}
+
+function prepare_problem_solver() {
+    echo -e "\033[1mPROBLEM-SOLVER\033[0m":
+    if [ -e "sc-machine/problem-solver" ]; then
+        cd problem-solver
+        git pull
+    else 
+        cd sc-machine
+        git clone https://github.com/semantic-pie/problem-solver
+        echo 'add_subdirectory(${SC_MACHINE_ROOT}/problem-solver)' >> CMakeLists.txt
+    fi
+    cd $WORKDIR
+}
+
+function prepare_sc_web() {
+    echo -e "\033[1mSC-WEB\033[0m":
+    if [ -e "sc-web" ]; then
+        cd sc-web
+        git pull
+    else
+        git clone https://github.com/ostis-ai/sc-web
+    fi
+    cd $WORKDIR
+}
+
+function prepare_sc_ims() {
+    echo -e "\033[1mKB.MINIMAL\033[0m":
+    if [ -e "ims.ostis.kb" ]; then
+        cd ims.ostis.kb
+        git pull
+    else
+         git clone https://github.com/semantic-pie/minimal_kb ims.ostis.kb
+    fi
+    cd $WORKDIR
+}
 
 
 case $1 in
@@ -22,35 +69,15 @@ case $1 in
 # clone components
 install)
     shift 1;
-
-    if [ -e "sc-web" ]; then
-        cd sc-web
-        echo SC-WEB:
-        git pull
-        cd ..
-    else
-        git clone https://github.com/ostis-ai/sc-web
-    fi
-
-    if [ -e "sc-machine" ]; then
-        cd sc-machine
-        echo SC-MACHINE:
-        git pull
-        cd ..
-    else
-         git clone https://github.com/ostis-ai/sc-machine
-    fi
-
-    if [ -e "ims.ostis.kb" ]; then
-        cd ims.ostis.kb
-        echo KB.MINIMAL:
-        git pull
-        cd ..
-    else
-         git clone https://github.com/semantic-pie/minimal_kb ims.ostis.kb
-    fi
+    prepare_sc_web
+    prepare_sc_machine
+    prepare_problem_solver
+    prepare_sc_ims
     ;;
-
+test)
+    shift 1;
+    echo $WORKDIR
+    ;;
 # show help
 --help)
     usage
